dbbd58325db3f96a389d91b8e048922a460e2513
jakemerola <jakemerola@gmail.com>
2021-03-04 17:14:32 -0500

    added code, client will not connect

--- experiment2/TCPClient.py
+++ experiment2/TCPClient.py
@@ -1,1 +1,30 @@
-pass
+import socket
+
+serverAddress="localhost"
+serverPort=11111
+
+class TCPClient:
+	def __init__(self):
+		clientSocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		try:
+			clientSocket.connect((serverAddress, serverPort))
+		except ConnectionRefusedError:
+			print("Error: Connection refused.")
+			exit(1)
+			
+
+		print("Type \"quit\" to exit the client, or \"shutdown\" to turn off server.")
+
+		while 1:
+			message=input("Enter a message: ")
+			clientSocket.send(message)
+			modifiedMessage=clientSocket.recv(1024)
+			print("Received message: %s" % modifiedMessage)
+
+			if ((message == "quit") or (message == "shutdown")):
+				print("Client exit")
+				break
+
+		clientSocket.close()
+
+TCPClient()
--- experiment2/TCPServerMultithread.py
+++ experiment2/TCPServerMultithread.py
@@ -1,1 +1,56 @@
-pass
+import socket
+import threading
+
+serverPort=11111
+serverHost="locahost"
+
+class TCPServerMultithread:
+	def __init__(self):
+		print("Using server port of %s" % serverPort)
+		running=True
+
+		try:
+			serverSocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)	
+			host=socket.gethostname()
+			print("Server is running on %s host" % socket.gethostbyname(host))
+			serverSocket.bind((host, serverPort))
+			serverSocket.listen(5)
+
+			while (running):
+				print("Threaded TCP server listening on port %s" % serverPort)
+				threading.Thread(target=self.talkToClient, args=(serverSocket.accept()))
+
+			print("Server shutting down")
+			serverSocket.shutdown(0)
+			serverSocket.close()
+
+		except IOError:
+			if (serverSocket):
+				serverSocket.close()
+			print("Error opening socket.")
+			exit(1)
+
+		except KeyboardInterrupt:
+			print("Keyboard interrupt")
+			if (serverSocket):
+				serverSocket.close()
+				print("Socket closed successfully.")
+			exit(1)
+
+
+	def talkToClient(connectionSocket, socket, address):
+		print("Accepted a connection from %s" % address)
+
+		while 1:
+			message=connectionSocket.recv(1024)
+			print("Received message: %s" % message)
+
+			connectionSocket.send(message.upper())
+
+			if (message == "shutdown"):
+				running=0
+			elif ((message == "quit") or (message == "shutdown")):
+				connectionSocket.close()
+				break
+
+TCPServerMultithread()
