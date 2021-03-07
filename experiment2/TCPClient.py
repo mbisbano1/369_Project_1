9cf5f50820f6fc5b379f85972cca24c1102b757c
jakemerola <jakemerola@gmail.com>
2021-03-06 20:54:52 -0500

    added argument support to server

--- experiment2/TCPClient.py
+++ experiment2/TCPClient.py
@@ -1,11 +1,28 @@
 import socket
-
-serverAddress="127.0.1.1"
-serverPort=11111
+import sys
 encoding="UTF-8"
 
 class TCPClient:
 	def __init__(self):
+		if (len(sys.argv) != 3):
+			print("Usage: python3 TCPClient.py server_address server_port")
+			exit(1)
+
+		serverAddress=sys.argv[1]
+		try:
+			serverPort=int(sys.argv[2])
+		except ValueError:
+			print("Error: input argument invalid.")
+			exit(1)
+
+		if ((serverPort <= 1024) or (serverPort >= 65536)):
+			print("Error: Server port value must be greater than 1024 and less than 65536.")
+			exit(1)
+
+		self.connect(serverAddress, serverPort)
+
+
+	def connect(self, serverAddress, serverPort):
 		try:
 			clientSocket=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			try:
@@ -38,6 +55,5 @@ class TCPClient:
 			if (clientSocket):
 				clientSocket.close()
 				print("Socket closed successfully.")
-
 
 TCPClient()
