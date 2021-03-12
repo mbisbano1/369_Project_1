85961565ba159ef36541aecb34335f5b756a1903
jakemerola <jakemerola@gmail.com>
2021-03-12 00:21:57 -0500

    starting socket programming

--- experiment5/UDPServer.c
+++ experiment5/UDPServer.c
@@ -1,6 +1,42 @@
+/*https://www.binarytides.com/socket-programming-c-linux-tutorial*/
 #include <stdio.h>
+#include <sys/socket.h>
 #define CR printf("\n")
 #define DEFAULT_PORT 12000
+
+int serverLoop(int serverPort)
+{
+	int socketDescriptor;
+
+	struct in_addr
+	{
+		unsigned long s_addr;
+	};
+
+	struct sockaddr_in
+	{
+		short sin_family;
+		unsigned short sin_port;
+		struct in_addr sin_addr;
+		char sin_zero[8];
+	};
+
+	struct sockaddr
+	{
+		unsigned short sa_family;
+		char sa_data[14];
+
+	};
+
+
+	socketDescriptor=socket(AF_INET, SOCK_STREAM, 0);
+	if (socketDescriptor == -1)
+	{
+		printf("Error: Could not create socket. Quitting."); CR; 
+		return(1);
+	}
+}
+
 
 int main(int argc, char * argv[])
 {
@@ -9,13 +45,9 @@ int main(int argc, char * argv[])
 	if (argc == 2)
 	{
 		/*convert input string to int*/
-		if (sscanf(argv[1], "%u", &serverPort) == 1)
+		if (sscanf(argv[1], "%u", &serverPort) != 1)
 		{
-			printf("Successfully converted to int %d" , serverPort); CR; 
-		}
-		else
-		{
-			printf("Error: Input value not an integer. Exiting."); CR; 
+			printf("Error: Input value not an integer. Quitting."); CR; 
 			return(1);
 		}
 	}
@@ -26,6 +58,5 @@ int main(int argc, char * argv[])
 		serverPort=DEFAULT_PORT;
 	}
 
-
-	return(0);
+	return(serverLoop(serverPort));
 }
