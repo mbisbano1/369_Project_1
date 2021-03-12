6684ead375f839d6a234d83cef4db39deea7a443
Michael Bisbano <46977425+mbisbano1@users.noreply.github.com>
2021-03-12 09:00:58 -0500

    Push for multiple user testing

--- experiment4/UDPpeer.py
+++ experiment4/UDPpeer.py
@@ -1,10 +1,10 @@
 import socket
 import sys, threading
 
-host_name = socket.gethostname()
-IP_ADDR = socket.gethostbyname(host_name)
-print("Host name is: ", host_name)
-print("IP ADDR is: ", IP_ADDR)
+#host_name = socket.gethostname()
+#IP_ADDR = socket.gethostbyname(host_name)
+#print("Host name is: ", host_name)
+#print("IP ADDR is: ", IP_ADDR)
 #HOST = socket.get_hostbyname(socket.get_hostname())
 
 #global server_address	# = ''
@@ -28,18 +28,26 @@ print("IP ADDR is: ", IP_ADDR)
 
 class UDPPeer:
 	"""UDP P2P Class/Object"""
+	hostname = ''			# Each peer has it's own hostname
 	addr = ''               # Each peer has it's own address
 	peersList = []          # Each peer maintains a list of other peers addresses.add()
 	ServerPort = 12000      # Each peer uses port 12000 for now?
 	ClientPort = 12001      # Each peer has it's output/client side port at 12001 for now
+	serverRunning = True	# Each peer has a status for it's server and client side
+	clientRunning = True	# ...
 	def __init__(self):
-		self.addr = input("Enter your IP Address from ifconfig: eg. 192.168.0.150")
-
+		#self.addr = input("Enter your IP Address from ifconfig: eg. 192.168.0.150")
+		self.hostname = socket.gethostname()
+		self.addr = socket.gethostbyname(self.hostname)
+		print("Hostname is: ", self.hostname)
+		print("IP address is: ", self.addr)
+		self.serverRunning = True
 		try:	# Try sending broadcast to scan for other peers on network
-			BroadcastS=socket(AF_INET, SOCK_DGRAM)
-			BroadcastS.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-			broadcastMessage = 'P2P_Messenger_Active' + self.addr
-			BroadcastS.sendto(broadcastMessage,('255.255.255.255', 12000))
+			BroadcastS=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			BroadcastS.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+			broadcastMessage = bytes(' ECE369 ' + self.addr, 'utf-8')
+			#BroadcastS.sendto(broadcastMessage,('255.255.255.255', 12000))
+			BroadcastS.sendto(broadcastMessage,('<broadcast>', 12000))
 			BroadcastS.close
 		except KeyboardInterrupt:
 			print("Keyboard Interrupt!")
@@ -50,9 +58,15 @@ class UDPPeer:
 
 
 		try:	# Try setting up the server
-			serverS=socket(AF_INET, SOCK_DGRAM)
+			serverS=socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 			serverS.bind(('',self.ServerPort))
 			print("ServerSide is ready to receive.")
+			while self.serverRunning:
+				message, clientAddress = serverS.recvfrom(2048)
+				print("Message Received: ")
+				print(message.decode())
+				ackMessage = 'Acknowledge Receipt'
+				serverS.sendto(ackMessage.encode(), clientAddress)
 		except KeyboardInterrupt:
 			print("Keyboard Interrupt!")
 			exit(1)
