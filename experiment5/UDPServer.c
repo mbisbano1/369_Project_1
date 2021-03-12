931ffb4c2fcd9384d98087cbc2faa098d09ff190
jakemerola <jakemerola@gmail.com>
2021-03-12 00:02:55 -0500

    get integer

--- experiment5/UDPServer.c
+++ experiment5/UDPServer.c
@@ -1,15 +1,31 @@
 #include <stdio.h>
 #define CR printf("\n")
+#define DEFAULT_PORT 12000
 
 int main(int argc, char * argv[])
 {
-	if (argc != 2)
+	unsigned int serverPort;
+
+	if (argc == 2)
+	{
+		/*convert input string to int*/
+		if (sscanf(argv[1], "%u", &serverPort) == 1)
+		{
+			printf("Successfully converted to int %d" , serverPort); CR; 
+		}
+		else
+		{
+			printf("Error: Input value not an integer. Exiting."); CR; 
+			return(1);
+		}
+	}
+	else
 	{
 		printf("Usage: ./UDPServer.c server_port"); CR; 
-		return(1);
+		printf("Using default server port value of %d", DEFAULT_PORT); CR; 
+		serverPort=DEFAULT_PORT;
 	}
 
-	printf("You entered a server port value of %s", argv[1]); CR;
 
 	return(0);
 }
