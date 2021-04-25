9e8f3bfc19c888db50ab10f8e3e96bee31b8cbbe
Michael Bisbano <46977425+mbisbano1@users.noreply.github.com>
2021-04-24 21:31:47 -0400

    First attempt at creating mdrive interface started here

---
+++ Final_Project_Mdrive_Access/MiddleGround.py
@@ -0,0 +1,25 @@
+import shutil
+import os, sys
+
+#Constant Definititions
+CWD = os.getcwd()
+source = CWD + '/MDrive'
+destination = CWD + '/Downloadables/mdrive.zip'
+
+#Function Definitions
+def make_zip(source, destination):
+        base = os.path.basename(destination)
+        name = base.split('.')[0]
+        format = base.split('.')[1]
+        archive_from = os.path.dirname(source)
+        archive_to = os.path.basename(source.strip(os.sep))
+        print(source, destination, archive_from, archive_to)
+        shutil.make_archive(name, format, archive_from, archive_to)
+        shutil.move('%s.%s'%(name,format), destination)
+
+
+
+#Beginning of code
+
+make_zip(source, destination)
+	
