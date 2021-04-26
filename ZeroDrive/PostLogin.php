e080d0c8b29dd904ca9b23c9cbc17f0159e43918
Michael Bisbano <46977425+mbisbano1@users.noreply.github.com>
2021-04-26 08:24:34 -0400

    FInal Tweaks

--- ZeroDrive/PostLogin.php
+++ ZeroDrive/PostLogin.php
@@ -4,13 +4,51 @@
    <title>
     MDRIVE ACCESS
    </title>
+   <style>
+    h2{text-align: center;}
+    
+    .container{
+	text-align: center;
+	padding-top: 10px;
+	padding-bottom: 10px;
+    }
+
+    p{text-align: center;}
+
+    body{font-family: Arial, Helvetica, sans-serif;}
+
+    a{text-align: center;}
+
+    button {
+	background-color: #3377FF;
+	color: white;
+	padding: 14px 20px;
+	margin: 8px 0;
+	border: none;
+	cursor: pointer;
+	width: 50%;
+    }
+    button:hover {
+	opacity: 0.8;
+
+    }
+
+   </style>
+
   </head>
-  <p>Click the RE-ZIP button and wait until you are notified that it is ready to download! </p>
+  <h2>Click the RE-ZIP button and wait until you are notified that it is ready to download! </h2>
+
   <form method="post">
-   <input type="submit" value="RE-ZIP" name="RE-ZIP"> 
+	<div class="container">
+	   <button type="submit" value="RE-ZIP" name="RE-ZIP">RE-ZIP the MDrive </button> 
+	</div>
   </form>
-  <br>
-  <a href="Downloadables/mdrive.zip" title="This is a link to the zipped m-drive">Download the MDrive zip folder</a> 
+
+  <p>
+  	<br>
+	 <a href="Downloadables/mdrive.zip" title="This is a link to the zipped m-drive">Download the MDrive zip folder</a> 
+	<br>
+  </p>
   <br>	
 
  </body>
@@ -24,7 +62,7 @@
 		$mDriveZip = shell_exec("sudo python3 /var/www/html/MiddleGround.py");
 		//echo"<pre>$mDriveZip</pre>";
 		echo"<br>";
-		echo"The MDrive Folder has successfully been zipped! You may now click the MDrive download link!";
+		echo"<p>The MDrive Folder has successfully been zipped! You may now click the MDrive download link!</p>";
 		echo"<br>";
 		
 	}
